c2f482918f142fb7dd9d3ebcbf2b921be9913c81
Ryuujinx <admin@failverse.com>
2018-12-03 00:58:59 -0600

    Day 3

---
+++ day3/day3-part2.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+
+	cloth := make([][]string,1100)
+	for y := range cloth {
+		cloth[y] = make([]string,1100)
+	}
+
+	f, err := os.Open("input")
+	if err != nil {
+		fmt.Print(err)
+	}
+	var conflict bool
+	var answer string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		l := scanner.Text()
+		specs := strings.Split(l, " ")
+		id := string(specs[0][1:])
+		pos := strings.Split(specs[2], ",")
+		xpos,_ := strconv.Atoi(pos[0])
+		ypos,_ := strconv.Atoi(strings.Split(pos[1],":")[0])
+		xsize,_ := strconv.Atoi(strings.Split(specs[3], "x")[0])
+		ysize,_ := strconv.Atoi(strings.Split(specs[3], "x")[1])
+
+
+		for xidx := xpos; xidx < xpos+xsize; xidx++ {
+			for yidx := ypos; yidx < ypos+ysize; yidx++ {
+				if cloth[xidx][yidx] == "" {
+					cloth[xidx][yidx] = id
+				} else {
+					cloth[xidx][yidx] = "O"
+				}
+			}
+		}
+
+
+	}
+	fmt.Println("foo")
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		fmt.Print(err)
+	}
+	scanner = bufio.NewScanner(f)
+	for scanner.Scan() {
+		l := scanner.Text()
+		specs := strings.Split(l, " ")
+		id := string(specs[0][1:])
+		pos := strings.Split(specs[2], ",")
+		xpos,_ := strconv.Atoi(pos[0])
+		ypos,_ := strconv.Atoi(strings.Split(pos[1],":")[0])
+		xsize,_ := strconv.Atoi(strings.Split(specs[3], "x")[0])
+		ysize,_ := strconv.Atoi(strings.Split(specs[3], "x")[1])
+		conflict = false
+		for xidx := xpos; xidx < xpos+xsize; xidx++ {
+			for yidx := ypos; yidx < ypos+ysize; yidx++ {
+				if cloth[xidx][yidx] == "O" {
+					 conflict = true
+					 break
+				}
+			}
+			if conflict {
+				break
+			}
+		}
+		if conflict == false {
+			answer = id
+			break
+		}
+	}
+	fmt.Println(answer)
+}
---
+++ day3/day3.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+
+	cloth := make([][]string,1100)
+	for y := range cloth {
+		cloth[y] = make([]string,1100)
+	}
+
+	f, err := os.Open("input")
+	if err != nil {
+		fmt.Print(err)
+	}
+	var conflict int
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		l := scanner.Text()
+		specs := strings.Split(l, " ")
+		id := string(specs[0][1:])
+		pos := strings.Split(specs[2], ",")
+		xpos,_ := strconv.Atoi(pos[0])
+		ypos,_ := strconv.Atoi(strings.Split(pos[1],":")[0])
+		xsize,_ := strconv.Atoi(strings.Split(specs[3], "x")[0])
+		ysize,_ := strconv.Atoi(strings.Split(specs[3], "x")[1])
+
+
+		for xidx := xpos; xidx < xpos+xsize; xidx++ {
+			for yidx := ypos; yidx < ypos+ysize; yidx++ {
+				if cloth[xidx][yidx] == "" {
+					cloth[xidx][yidx] = id
+				} else {
+					cloth[xidx][yidx] = "O"
+				}
+			}
+		}
+
+
+	}
+
+
+	for _,x := range cloth {
+		for _,y := range x {
+			if y == "O" {
+				conflict = conflict + 1
+			}
+		}
+	}
+
+fmt.Println(conflict)
+}
